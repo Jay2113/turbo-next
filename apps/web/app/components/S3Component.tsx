5668c1fb6cbe6e5bd522274d9f21acded7c37c3c
Jay2113 <jay.ambrish.raval@gmail.com>
2025-02-14 02:30:07 -0500

    set Image component to unoptimized

--- apps/web/app/components/S3Component.tsx
+++ apps/web/app/components/S3Component.tsx
@@ -34,6 +34,7 @@ export default function WelcomeMessage() {
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     priority
                     onError={() => setImageError(true)}
+                    unoptimized
                   />
                 </div>
               )}
